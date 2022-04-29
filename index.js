7255ebc05bbad681b295cc6bdac57cff2cfb5f45
suzan <asadsuzan7@gmail.com>
2022-04-29 15:19:15 +0600

    conected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // set middlewareWrapper
 app.use(cors());
 app.use(express.json());
+
+// conection strin
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o7mmp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// conect to db
+
+async function run() {
+  try {
+    await client.connect();
+    const carCollections = client.db("managcar").collection("cars");
+
+    console.log("ok");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("welcome to car manager server");
